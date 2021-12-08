e000b872a97cf5af352938427cbe6402b2a35891
htwenty-1 <h2ne1.public@gmail.com>
2021-12-08 21:12:25 +0900

    2021.12.08.(Wed)

--- 4th Java/20211208/HomeWork/src/MainClass.java
+++ 4th Java/20211208/HomeWork/src/MainClass.java
@@ -6,75 +6,72 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		/*
-			사용자가 원하는 학생 수로 점수를 입력받는다.
-			총점, 평균, 그리고 최고점수를 구한다.
-			입력받은 점수 층에서 90점 이상의 학생 수는 몇명인가?
-			그리고 그 점수들만을 새로운 배열에 저장하는 프로그램을 작성하시오.
+			학생 수만큼 점수를 입력받아 점수의 통계를 내려고 합니다.
+      이 때, 총점, 평균, 그리고 최고점수를 구하고 입력 받은 점수 중에 90점 이상은 몇 명인지 출력해주는 프로그램을 작성해주세요. 그리고 90점 이상을 맞은 점수를 새로운 배열에 넣어주세요.
 		*/
-		
 
-		// 필요 변수 선언하기
+		// 필요한 변수 선언
 		Scanner input = new Scanner(System.in);
-		int students, studentsArr[], count;
-		int sum, avg, max;
-		int over90[];
-		
-		int faviNum, faviNumArr[];
+		int count;
+		int scoreArr[] = null;
+		int score;
+		double avg;
+		int max;
+		int high;
 		
 		// 학생 수 입력 받기
-		System.out.print("학생 수를 입력하세요 : ");
-		students = input.nextInt();
-		
-		// 학생 수만큼 점수 입력 받아 배열에 넣어주기
-		
-		studentsArr = new int[students];
-		
-		for (int i = 0; i < studentsArr.length; i++) {
-			System.out.print((i+1) + "번째 학생의 점수 : ");
-			studentsArr[i] = input.nextInt();
+		System.out.print("학생 수를 입력해주세요 : ");
+		count = input.nextInt();
+
+		scoreArr = new int [count];	// 배열에 학생수만큼 원소를 갖겠다고 명시
+		// 학생 수만큼 점수 입력 받기
+		for (int i = 0; i < count; i++) {
+			System.out.print((i+1) + "번 째 학생의 점수를 입력해주세요 : ");
+			scoreArr[i] = input.nextInt();
 		}
-		
-		// 총점, 평균, 최고점수 구하여 출력하기
-		sum = 0;
-		avg = 0;
-		max = studentsArr[0];
-		for (int j = 0; j < studentsArr.length; j++) {
-			sum += studentsArr[j];
-			avg = sum / studentsArr.length;
-			if (studentsArr[j] > max) {
-				max = studentsArr[j];
+
+		// 총점 산출하기
+		score = 0;
+		for (int i = 0; i < scoreArr.length; i++) {
+			score += scoreArr[i];
+		}
+
+		// 평균 산출하기
+		// 주의사항!!! 부동소수점!!
+		avg = (double)score / scoreArr.length;
+
+		// 최고 점수 구하기(최댓값)
+		max = scoreArr[0];
+		for (int i = 0; i < scoreArr.length; i++) {
+			if (scoreArr[i] > max) {
+				max = scoreArr[i];
 			}
-			
 		}
-		System.out.println("입력한 점수의 총점은 " + sum + "점 입니다.");
+
+		// 90점이상 득점자 몇명인지
+		high = 0;
+		for (int i = 0; i < scoreArr.length; i++) {
+			if (scoreArr[i] >= 90) {
+				high++;
+			}
+		}
+
+		// 90이상의 수를 새로운 숫자 배열에 넣기
+		int highScore[] = new int[high];
+		int n = 0;
+		for (int i = 0; i < scoreArr.length; i++) {
+			if (scoreArr[i] >= 90) {
+				highScore[n] = scoreArr[i];
+			}
+		}
+
+		// 모두 출력하기
+		System.out.println("입력 받은 점수는 " + Arrays.toString(scoreArr) + "입니다.");
+		System.out.println("입력한 점수의 총점은 " + score + "점 입니다.");
 		System.out.println("입력한 점수의 평균은 " + avg + "점 입니다.");
-		System.out.println("입력한 점수 중 최대값은 " + max + "점 입니다.");
-		
-		// 90점 이상인 학생 수 출력하기
-		count = 0;
-		for (int k = 0; k < studentsArr.length; k++) {
-			if (studentsArr[k] >= 90) {
-				count++;
-			}
-		}
-		System.out.println("90점 이상인 학생 수는 " + count + "명입니다.");
-		
-		//////////////////////////////////////
-		
-		/*
-		   5개의 숫자를 입력받는 프로그램을 작성하시오.
-		   단, 음수가 입력되면 다시 입력받게 한다.
-		 */
-		
-		
-		/*
-		for (int s = 0; s < 5; s++) {
-			faviNumArr = new int[s];
-			System.out.print((s+1) + "번째 숫자를 입력해주세요: ");
-			faviNumArr[s] = input.nextInt();
-			System.out.println("당신이 입력하고 저정한 숫자는 " + faviNumArr[i] + "입니다!");
-		}
-		*/
+		System.out.println("입력한 점수 중 최고점수는 " + max + "점 입니다.");
+		System.out.println("90점 이상 득점자는 " + high + "명 입니다.");
+		System.out.println(Arrays.toString(highScore));
 	}
 
 }
